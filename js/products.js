97c40ed74e9e06d0aac07086c2b68db0b356b672
Tomas Maneri <tomasabelmaneri@gmail.com>
2024-06-15 09:05:32 -0300

    first commit

---
+++ js/products.js
@@ -0,0 +1,152 @@
+const products = [
+    {
+        id: "RareSneaker-01",
+        title: "Rare Sneaker 01",
+        image: "./img/rare-sneakers-01.jpg",
+        category: {
+            name: "Rare Sneaker",
+            id: "RareSneaker"
+        },
+        price: 400
+    },
+    {
+        id: "RareSneaker-02",
+        title: "Rare Sneaker 02",
+        image: "./img/rare-sneakers-02.jpg",
+        category: {
+            name: "Rare Sneaker",
+            id: "RareSneaker"
+        },
+        price: 480
+    },
+    {
+        id: "RareSneaker-03",
+        title: "Rare Sneaker 03",
+        image: "./img/rare-sneakers-04.jpg",
+        category: {
+            name: "Rare Sneaker",
+            id: "RareSneaker"
+        },
+        price: 380
+    },
+    {
+        id: "RareSneaker-04",
+        title: "Rare Sneaker 04",
+        image: "./img/rare-sneakers-04.jpg",
+        category: {
+            name: "Rare Sneaker",
+            id: "RareSneaker"
+        },
+        price: 185
+    },
+    {
+        id: "RareSneaker-05",
+        title: "Rare Sneaker 05",
+        image: "./img/rare-sneakers-05.jpg",
+        category: {
+            name: "Rare Sneaker",
+            id: "RareSneaker"
+        },
+        price: 200
+    },
+    {
+        id: "sporty-sneaker-01",
+        title: "Sporty Sneaker 01",
+        image: "./img/sporty-sneakers-01.jpg",
+        category: {
+            name: "Sporty Sneaker",
+            id: "SportySneaker"
+        },
+        price: 700
+    },
+    {
+        id: "sporty-sneaker-02",
+        title: "Sporty Sneaker 02",
+        image: "./img/sporty-sneakers-02.jpg",
+        category: {
+            name: "Sporty Sneaker",
+            id: "SportySneaker"
+        },
+        price: 350
+    },
+    {
+        id: "sporty-sneaker-03",
+        title: "Sporty Sneaker 03",
+        image: "./img/sporty-sneakers-03.jpg",
+        category: {
+            name: "Sporty Sneaker",
+            id: "SportySneaker"
+        },
+        price: 175
+    },
+    {
+        id: "sporty-sneaker-04",
+        title: "Sporty Sneaker 04",
+        image: "./img/sporty-sneakers-04.jpg",
+        category: {
+            name: "Sporty Sneaker",
+            id: "SportySneaker"
+        },
+        price: 500
+    },
+    {
+        id: "sporty-sneaker-05",
+        title: "Sporty Sneaker 05",
+        image: "./img/sporty-sneakers-05.jpg",
+        category: {
+            name: "Sporty Sneaker",
+            id: "SportySneaker"
+        },
+        price: 280
+    },
+    {
+        id: "urban-sneaker-01",
+        title: "Urban Sneaker 01",
+        image: "./img/urban-style-sneakers-01.jpg",
+        category: {
+            name: "Urban Sneaker",
+            id: "UrbanSneaker"
+        },
+        price: 650
+    },
+    {
+        id: "urban-sneaker-02",
+        title: "Urban Sneaker 02",
+        image: "./img/urban-style-sneakers-02.jpg",
+        category: {
+            name: "Urban Sneaker",
+            id: "UrbanSneaker"
+        },
+        price: 780
+    },
+    {
+        id: "urban-sneaker-03",
+        title: "Urban Sneaker 03",
+        image: "./img/urban-style-sneakers-03.jpg",
+        category: {
+            name: "Urban Sneaker",
+            id: "UrbanSneaker"
+        },
+        price: 360
+    },
+    {
+        id: "urban-sneaker-04",
+        title: "Urban Sneaker 04",
+        image: "./img/urban-style-sneakers-04.jpg",
+        category: {
+            name: "Urban Sneaker",
+            id: "UrbanSneaker"
+        },
+        price: 220
+    },
+    {
+        id: "urban-sneaker-05",
+        title: "Urban Sneaker 05",
+        image: "./img/urban-style-sneakers-05.jpg",
+        category: {
+            name: "Urban Sneaker",
+            id: "UrbanSneaker"
+        },
+        price: 480
+    }
+];
