d4a3e20572bae544e7f21ec6f645e4807879dead
Tomas Maneri <tomasabelmaneri@gmail.com>
2024-06-18 21:00:17 -0300

    all bad

---
+++ js/cart.js
@@ -0,0 +1,58 @@
+// WE DECLARE LOCAL STORAGE //
+const productsInCart = JSON.parse(localStorage.getItem("products-in-cart"));
+
+// WE DECLARE DOM
+const containerEmptyCart = document.querySelector("#empty-cart-title");
+const containerProductsCart = document.querySelector("#container-products-cart");
+const containerCartActions = document.querySelector("#cart-actions");
+const ContainerCartPurchased = document.querySelector("#cart-purchased");
+
+
+
+
+
+// CART IF //
+if (productsInCart) {
+
+    containerEmptyCart.classList.add("disabled");
+    containerProductsCart.classList.remove("disabled");
+    containerCartActions.classList.remove("disabled");
+    ContainerCartPurchased.classList.add("disabled");
+
+    productsInCart.forEach(product => {
+
+        const div  = document.createElement("div");
+        div.classList.add("product-products-cart");
+        div.innerHTML = `
+            <div class="img-product-products-cart">
+                <img src="${product.image}" alt="${product.title}">
+            </div>
+            <div class="product-name-cart">
+                <small>Product title</small>
+                <h1>${product.title}</h1>
+            </div>
+            <div class="product-amount-cart">
+                <small>Amount</small>
+                <p>${product.amount}</p>
+            </div>
+            <div class="product-cart-price">
+                <small>Price</small>
+                <p>${product.price}</p>
+            </div>
+            <div class="cart-subtotal">
+                <small>Subtotal</small>
+                <p>${product.price * product.amount}</p>
+            </div>
+            <button class="button-trash-cart" id="${product.id}"><i class="bi bi-trash3"></i><button>
+        `
+        containerProductsCart.append(div);
+
+    });
+
+    
+
+
+} else {
+
+}
+
